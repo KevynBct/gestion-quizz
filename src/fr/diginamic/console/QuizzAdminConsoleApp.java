0a232127b00133b9bc972e1bc299a1d959d00519
Kevyn <kevyn.buchet@gmail.com>
2018-11-14 17:31:30 +0100

    Implémentations des fonctionnalitées

--- src/fr/diginamic/console/QuizzAdminConsoleApp.java
+++ src/fr/diginamic/console/QuizzAdminConsoleApp.java
@@ -1,11 +1,18 @@
 package fr.diginamic.console;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import fr.diginamic.model.Question;
+
 public class QuizzAdminConsoleApp {
+	public static ArrayList<Question> listeQuestions;
 
 	public static void main(String[] args) {
 		
+		listeQuestions = new ArrayList<Question>();
+		
+		initQuizz();
 		afficheMenu();
 
 	}
@@ -19,35 +26,146 @@ public class QuizzAdminConsoleApp {
 				"99. Sortir");
 		
 		Scanner scanner = new Scanner(System.in);
-		int choice = scanner.nextInt();
+		int choice = Integer.parseInt(scanner.nextLine());
+		//scanner.close();
 		
 		switch (choice) {
 			case 1 : {
 				System.out.println("Liste des questions :\n");
+				afficherQuestions();
 				afficheMenu();
 				break;
 			}
 			case 2 : {
 				System.out.println("Ajout d’une nouvelle question :\n");
+				ajouterQuestion();
 				afficheMenu();
 				break;
 			}
 			case 3 : {
 				System.out.println("Suppression d’une question :\n");
+				supprimerQuestion();
 				afficheMenu();
 				break;
 			}
 			case 4 : {
 				System.out.println("Exécution du quizz :\n");
+				executerQuizz();
 				afficheMenu();
 				break;
 			}
 			case 99 : {
-				System.out.println("Aurevoir");
+				System.out.println("Au revoir");
+				break;
+			}
+			default : {
+				System.out.println("Veuillez choisir une proposition\n");
+				afficheMenu();
 				break;
 			}
 		}
 		
 	}
-
+	
+	public static void afficherQuestions() {
+		if(listeQuestions.isEmpty()) {
+			System.out.println("Il n'y a pas de question");
+		}else {
+			int index = 0;
+			for(Question question : listeQuestions) {
+				index++;
+				System.out.println(index+") "+question.toString());
+			}
+		}
+	}
+	
+	public static void ajouterQuestion() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir l’intitulé de la question :\n");
+		String intitule = scanner.nextLine();
+		
+		System.out.println("Veuillez saisir le nombre de propositions : (exemple : 3)\n");
+		int nbReponse = Integer.parseInt(scanner.nextLine());
+		
+		Question question = new Question(intitule, nbReponse);
+		
+		for(int i = 1 ; i <= nbReponse; i++) {
+			System.out.println("Veuillez saisir la proposition de réponse n°"+i+" :");
+			question.addProposition(scanner.nextLine());
+		}
+		
+		System.out.println("Veuillez saisir le numéro de la bonne réponse (entre 1 et "+nbReponse+") :\n");
+		int rep = Integer.parseInt(scanner.nextLine());
+		while(rep > nbReponse || rep < 1) {
+			System.out.println("Saisissez un numéro entre 1 et "+nbReponse+"\n");
+			rep = Integer.parseInt(scanner.nextLine());
+		}
+		question.setBonneReponse(rep);
+		
+		listeQuestions.add(question);
+		System.out.println("La question est ajoutée");
+		
+		
+	}
+	
+	public static void supprimerQuestion() {
+		System.out.println("Veuillez choisir le numéro de la question à supprimer.");
+		
+		Scanner scanner = new Scanner(System.in);
+		int rep = Integer.parseInt(scanner.nextLine());
+		while(rep > listeQuestions.size() || rep < 1) {
+			System.out.println("Il n'y a pas de question "+rep+"\nChoisissez en une autre");
+			rep = Integer.parseInt(scanner.nextLine());
+		}
+		listeQuestions.remove(rep-1);
+	}
+	
+	public static void executerQuizz() {
+		if(listeQuestions.isEmpty()) {
+			System.out.println("Il n'y a pas de quizz");
+		}else {
+			int index = 0;
+			int score = 0;
+			Scanner scanner = new Scanner(System.in);
+			for(Question question : listeQuestions) {
+				index++;
+				System.out.println(index+") "+question.toString()+"\nRéponse : ");
+				if(question.verifierReponse(Integer.parseInt(scanner.nextLine()))){
+					System.out.println("Bonne réponse");
+					score++;
+				}else {
+					System.out.println("Mauvaise réponse, la bonne réponse était : "+question.getPropositions().get(question.getBonneReponse()-1));
+				}				
+			}
+			System.out.println("Score final : "+score+"/"+listeQuestions.size());
+		}
+	}
+	
+	public static void initQuizz() {
+		Question q1 = new Question("Capitale de la France", 4);
+		q1.addProposition("Marseille");
+		q1.addProposition("Paris");
+		q1.addProposition("Lille");
+		q1.addProposition("Nantes");
+		q1.setBonneReponse(2);
+		
+		Question q2 = new Question("Capitale de l'Espagne", 4);
+		q2.addProposition("Madrid");
+		q2.addProposition("Barcelone");
+		q2.addProposition("Valence");
+		q2.addProposition("Bilbao");
+		q2.setBonneReponse(1);
+		
+		Question q3 = new Question("Capitale du Japon", 4);
+		q3.addProposition("Kyoto");
+		q3.addProposition("Nara");
+		q3.addProposition("Osaka");
+		q3.addProposition("Tokyo");
+		q3.setBonneReponse(4);
+		
+		listeQuestions.add(q1);
+		listeQuestions.add(q2);
+		listeQuestions.add(q3);
+	}
 }
--- src/fr/diginamic/model/Question.java
+++ src/fr/diginamic/model/Question.java
@@ -1,39 +1,59 @@
 package fr.diginamic.model;
 
+import java.util.ArrayList;
+
 public class Question {
 	private String intitule;
-	private String[] propositions;
-	private String bonneReponse;
+	private ArrayList<String> propositions;
+	private int bonneReponse;
 	
 	public Question(String intitule, int nbReponse) {
 		this.intitule = intitule;
-		this.propositions = new String[nbReponse];
+		this.propositions = new ArrayList<String>(nbReponse);
 	}
 	
-	public Boolean verifierReponse(String reponse){
+	public Boolean verifierReponse(int reponse){
 		if (this.bonneReponse == reponse) 
 			return true;
 		else
 			return false;
 	}
 	
+	public void addProposition(String proposition) {
+		this.propositions.add(proposition);
+	}
+
 	public String getIntitule() {
 		return intitule;
 	}
+
 	public void setIntitule(String intitule) {
 		this.intitule = intitule;
 	}
-	public String[] getPropositions() {
+
+	public ArrayList<String> getPropositions() {
 		return propositions;
 	}
-	public void setPropositions(String[] propositions) {
+
+	public void setPropositions(ArrayList<String> propositions) {
 		this.propositions = propositions;
 	}
-	public String getBonneReponse() {
+
+	public int getBonneReponse() {
 		return bonneReponse;
 	}
-	public void setBonneReponse(String bonneReponse) {
+
+	public void setBonneReponse(int bonneReponse) {
 		this.bonneReponse = bonneReponse;
 	}
+	
+	public String toString() {
+		String listeReponse ="\n";
+		for(String rep : propositions) {
+			listeReponse += "\t- "+rep+"\n";
+		}
+		return this.intitule+listeReponse;
+	}
+	
 
 }
